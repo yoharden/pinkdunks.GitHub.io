0dc049599cfee74f7db642dc4550d92822c1ac6c
li jason <jl0301429@gmail.com>
2023-09-13 17:02:09 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to toggle an element's visibility
+function toggleVisibility(id) {
+  var element = document.getElementById(id);
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
